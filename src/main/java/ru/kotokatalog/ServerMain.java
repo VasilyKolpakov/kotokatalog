d782d176c3fccf245602a5b8488f24cfb6e4c17d
Vasily Kolpakov <vasilykolpakov@gmail.com>
2021-09-05 21:57:11 +0300

    velocity templates

--- src/main/java/ru/kotokatalog/ServerMain.java
+++ src/main/java/ru/kotokatalog/ServerMain.java
@@ -1,19 +1,31 @@
 package ru.kotokatalog;
 
 import com.sun.net.httpserver.HttpServer;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class ServerMain {
+    static {
+        // init Velocity
+        Properties properties = new Properties();
+        properties.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        properties.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        Velocity.init(properties);
+    }
+
     public static void main(String[] args) throws IOException, SQLException {
         final String dbFile;
         if (args.length > 0) {
@@ -31,48 +43,33 @@ public class ServerMain {
         InetSocketAddress socketAddress = new InetSocketAddress("localhost", 8080);
         var server = HttpServer.create(socketAddress, 10);
         server.createContext("/", exchange -> {
-            var catsHtml = new StringBuilder();
+            var cats = new ArrayList<String>();
             try (var statement = conn.createStatement()) {
                 statement.execute("select name from cat");
                 var rs = statement.getResultSet();
                 while (rs.next()) {
                     var name = rs.getString(1);
-                    catsHtml.append("<p>");
-                    catsHtml.append(name);
-                    catsHtml.append("</p>\n");
+                    cats.add(name);
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
                 System.exit(1);
             }
-            String html = "<!DOCTYPE html>\n" +
-                    "<html>\n" +
-                    "<body>\n" +
-                    "<h1>Котокаталог</h1>\n" +
-                    catsHtml +
-                    "<a href=\"/add\">добавить кота</a>\n" +
-                    "</body>\n" +
-                    "</html>\n";
-            byte[] htmlBytes = html.getBytes(StandardCharsets.UTF_8);
+            VelocityContext context = new VelocityContext();
+            context.put("cats", cats);
+            var template = Velocity.getTemplate("index.v.html");
+            StringWriter sw = new StringWriter();
+            template.merge(context, sw);
+            byte[] htmlBytes = sw.toString().getBytes(StandardCharsets.UTF_8);
             exchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
             exchange.sendResponseHeaders(200, htmlBytes.length);
             exchange.getResponseBody().write(htmlBytes);
             exchange.close();
         });
         server.createContext("/add", exchange -> {
-            String html = """
-                    <!DOCTYPE html>
-                    <html>
-                    <body>
-
-                    <form action="/add" method="post">
-                      <label for="name">Имя:</label><br>
-                      <input type="text" id="name" name="name" value=""><br>
-                      <input type="submit" value="Добавить">
-                    </form>
-                    </body>
-                    </html>""";
-            byte[] htmlBytes = html.getBytes(StandardCharsets.UTF_8);
+            var htmlStream = ServerMain.class.getClassLoader().getResourceAsStream("add_cat.html");
+            assert htmlStream != null;
+            byte[] htmlBytes = readStreamAsBytes(htmlStream);
             if (exchange.getRequestMethod().equals("GET")) {
                 exchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
                 exchange.sendResponseHeaders(200, htmlBytes.length);
@@ -103,5 +100,11 @@ public class ServerMain {
         });
         server.start();
         System.out.printf("http://%s:%d/%n", socketAddress.getHostName(), socketAddress.getPort());
+    }
+
+    static byte[] readStreamAsBytes(InputStream src) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        src.transferTo(out);
+        return out.toByteArray();
     }
 }
